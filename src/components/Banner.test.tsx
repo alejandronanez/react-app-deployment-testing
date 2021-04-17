debce8ae1dabe2c81682f086c46fa74e7c2c292f
Alejandro Nanez <alejonanez@gmail.com>
2021-04-17 13:38:05 -0500

    More tests

--- src/components/Banner.test.tsx
+++ src/components/Banner.test.tsx
@@ -1,6 +1,19 @@
-describe('Banner Component', () => {
-  it.todo('should turn the switch off');
-  it.todo('should turn the switch on');
-});
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Banner } from './Banner';
 
-export {};
+describe('Banner Component', () => {
+  test('the switch should start off', () => {
+    render(<Banner />);
+    expect(screen.getByText(/switch is/i)).toHaveTextContent(/off/i);
+  });
+  test('clicking the button should toggle the switch', () => {
+    render(<Banner />);
+    const bannerLabel = screen.getByText(/switch is/i);
+    const button = screen.getByRole('button');
+
+    fireEvent.click(button);
+    expect(bannerLabel).toHaveTextContent(/on/i);
+    fireEvent.click(button);
+    expect(bannerLabel).toHaveTextContent(/off/i);
+  });
+});
