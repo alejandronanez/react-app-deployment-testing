554aa2f8c78d0e0021dfd81af2fcac43adfe0f77
Alejandro Nanez <alejonanez@gmail.com>
2021-04-17 13:38:34 -0500

    Used the component in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Banner } from './components/Banner';
 
 function App() {
-  return <div>Hello from template</div>;
+  return (
+    <div className="bg-gray-50 h-screen">
+      <div className="max-w-sm mx-auto py-8 px-4 h-52">
+        <Banner />
+      </div>
+    </div>
+  );
 }
 
 export default App;
